53623a93ec53d68e05183ef4369d1cd871339b8b
TheoPinardin <92739527+TheoPinardin@users.noreply.github.com>
2023-04-19 01:42:56 +0200

    version finale

--- main.py
+++ main.py
@@ -1,72 +1,79 @@
-from selenium import webdriver
-import time
-import json
-from selenium.webdriver.common.by import By
+import datetime
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import twitchdriver
+import json
 
-driver = webdriver.Chrome('chromedriver')
-driver.get("https://www.twitch.tv/directory?sort=VIEWER_COUNT")
+def main():
+    driver = twitchdriver.TwitchDriver()
+    data = driver.get_categories(5)
 
-Game_data = dict()
-Game_data['Category'] = []
-Game_data['Viewer'] = []
-Game_data['Tag'] = []
-Game_data['Link'] = []
-def Viewer2Int(Viewer):
-    Viewer = Viewer.replace(",", ".")
-    if "k" in Viewer:
-        return float(Viewer.split(" k")[0])*1000
-    else:
-        return float(Viewer.split(" spec")[0])
+    Category_data = dict()
+    Category_data['Category'] = data[0]
+    Category_data['Viewer'] = data[1]
+    Category_data['Tag'] = data[2]
+    Category_data['Link'] = data[3]
+    langues = getViewerPerLanguage(driver, Category_data)
+    Language_data = dict()
+    Language_data['Language'] = []
+    Language_data['NumberOfViewer'] = []
+    for i in langues:
+        Language_data['Language'].append(i[0])
+        Language_data['NumberOfViewer'].append(i[1])
 
+    createJSON(Language_data,'sample2')
+    createJSON(Category_data,'sample')
 
-def getTag(i):
-    path_category1 = '//*[@id="browse-root-main-content"]/div[4]/div/div[1]/div[' + str(i) + ']/div/div/div/div/div[2]/div/div[1]/button/div/div/span'
-    path_category2 = '//*[@id="browse-root-main-content"]/div[4]/div/div[1]/div[' + str(i) + ']/div/div/div/div/div[2]/div/div[2]/button/div/div/span'
-    path_category3 = '//*[@id="browse-root-main-content"]/div[4]/div/div[1]/div[' + str(i) + ']/div/div/div/div/div[2]/div/div[3]/button/div/div/span'
-    Tags = []
-    try:
-        Tags.append(driver.find_element(By.XPATH,path_category1).text)
-        Tags.append(driver.find_element(By.XPATH, path_category2).text)
-        Tags.append(driver.find_element(By.XPATH, path_category3).text)
-    except:
-        return Tags
-    return Tags
+    dataviz("sample.json")
+    plot_langues_vues("sample2.json")
 
-def getCategory(n):
-    for i in range(2,n + 2):
-        path_name = '//*[@id="browse-root-main-content"]/div[4]/div/div[1]/div['+str(i)+']/div/div/div/div/div[1]/div/div/div/div/span/a/h2'
-        path_viewer = '//*[@id="browse-root-main-content"]/div[4]/div/div[1]/div['+str(i)+']/div/div/div/div/div[1]/div/div/p/a'
-        path_link = '//*[@id="browse-root-main-content"]/div[4]/div/div[1]/div['+str(i)+']/div/div/div/div/div[1]/div/div/div/div/span/a'
+def createJSON(data,nom):
+    with open(nom + '.json','w') as outfile:
+        json.dump(data, outfile, indent = 4)
 
-        Game_data['Category'].append(driver.find_element(By.XPATH,path_name).text)
-        Game_data['Viewer'].append(Viewer2Int(driver.find_element(By.XPATH,path_viewer).text))
-        Game_data['Tag'].append(getTag(i))
-        Game_data['Link'].append(driver.find_element(By.XPATH,path_link).get_attribute('href'))
-        time.sleep(1)
+def getViewerPerLanguage(driver, data):
+    data_top_streams = driver.get_top_streams(data['Link'], 10)
+    langues = [["Français", 0], ["English", 0], ["Deutsch", 0], ["Español", 0], ["Italiano", 0], ["中文", 0], ["日本語", 0], ["العربية", 0]]
+    for cat in data_top_streams:
+        for stream in cat:
+            for i in range(len(langues)):
+                if(langues[i][0] in stream[2]):
+                    langues[i][1] += stream[1]
+    return langues
 
-def createJSON(data):
-    with open('sample.json','w') as outfile:
-        json.dump(data, outfile, indent=4)
 
 def dataviz(json_file):
     df = pd.read_json(json_file)
     plt.figure(figsize=(15, 20))
     sns.set(rc={'figure.figsize': (15, 20)})
     sns.barplot(x='Category', y='Viewer', data=df)
-    plt.xticks(rotation=45, ha='right', fontsize=16)
-    plt.yticks(fontsize=16)
-    plt.title('Viewer by Category', fontsize=25)
-    plt.xlabel('Category', fontsize=20)
-    plt.ylabel('Viewer', fontsize=20)
+    plt.xticks(rotation=45, ha='right', fontsize=36)
+    plt.yticks(fontsize=36)
+
+    now = datetime.datetime.now()
+    formatted_now = now.strftime("le %d/%m/%y à %Hh%Mmin%Ss")
+    plt.title('Viewer by Category ' + formatted_now, fontsize=45)
+    plt.xlabel('Category', fontsize=40)
+    plt.ylabel('Viewer', fontsize=40)
+    sns.despine()  # Remove the top and right spines
     plt.show()
 
-def main():
-    getCategory(10)
-    print(Game_data)
-    createJSON(Game_data)
-    dataviz('sample.json')
-main()
+def plot_langues_vues(json_file):
+    df = pd.read_json(json_file)
+    plt.figure(figsize=(15, 20))
+    sns.set(rc={'figure.figsize': (15, 20)})
+    sns.barplot(x='Language', y='NumberOfViewer', data=df)
+    plt.xticks(rotation=45, ha='right', fontsize=36)
+    plt.yticks(fontsize=36)
 
+    now = datetime.datetime.now()
+    formatted_now = now.strftime("le %d/%m/%y à %Hh%Mmin%Ss")
+    plt.title('Viewer by Language ' + formatted_now, fontsize=45)
+    plt.xlabel('Language', fontsize=40)
+    plt.ylabel('NumberOfViewer', fontsize=40)
+    sns.despine()  # Remove the top and right spines
+    plt.show()
+
+
+main()
